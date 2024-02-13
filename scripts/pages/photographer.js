6e13845a0a81c2c0899ee7ffc2fce25043bd619f
mohamednourbenali <mohamednourbenali@gmail.com>
2024-02-13 17:08:12 +0100

    affichage des portraits du photographe

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -26,3 +26,42 @@
     photographHeader.innerHTML += description;
     document.querySelector(".portraits").innerHTML+=photographImage;
 
+    // récupérer les portrait du photographe
+    const searchPhotos = result.media;
+    const photos = searchPhotos.filter(item => item.photographerId === photographId);
+
+    // Afficher les protrait du photographe
+    function displayWork (liste){
+        const photographerWork = document.querySelector(".photographer-work");
+        photographerWork.innerHTML="";
+        for (let i=0; i<liste.length;i++){
+            if(liste[i].image==undefined){
+                photographerWork.innerHTML+=`
+                <div class="portrait">
+                    <video controls style="width : 100%"; height:100%;">
+                        <source src="${liste[i].video}" type="video/mp4">
+                    </video>
+                    <div class="description-likes">
+                        <p class="title">${liste[i].title}</p>
+                        <div class="like">
+                            <p>${liste[i].likes}</p>
+                            <i class="fa-solid fa-heart"></i>
+                        </div>
+                    </div>
+                </div>`;
+            }else{
+                photographerWork.innerHTML+=`
+                <div class="portrait">
+                    <img src="${liste[i].image}" class="hover-shadow">
+                    <div class="description-likes">
+                        <p class="title">${liste[i].title}</p>
+                        <div class="like">
+                            <p>${liste[i].likes}</p>
+                            <i class="fa-solid fa-heart"></i>
+                        </div>
+                    </div>
+                </div>`;
+            }
+        }
+    }
+    displayWork(photos);
